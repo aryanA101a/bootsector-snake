85b0a1ebc9c54f3fe880ee21f37b5e3750399fbb
Aryan Arora <aryanarora.w1@gmail.com>
2023-04-24 13:09:55 +0530

    snake movement

--- snake.asm
+++ snake.asm
@@ -14,6 +14,10 @@ SNAKECOLOR = 7020h
 TIMER = 046Ch ;timer ticks handled in bios data area at this location in ram
 SNAKEXARRAY = 1000h
 SNAKEYARRAY = 2000h
+UP = 0
+DOWN = 1
+LEFT = 2
+RIGHT = 3
 
 ;;VARIABLES
 playerX: dw 40
@@ -65,10 +69,62 @@ game_loop:
     mov ax, APPLECOLOR
     stosw
 
+    ;;Move snake in current direction
+    mov al, [direction]
+    cmp al,UP
+    je move_up
+    cmp al,DOWN
+    je move_down
+    cmp al,LEFT
+    je move_left
+    cmp al,DOWN
+    je move_down
+
+    jmp update_snake_body
+
+    move_up:
+        dec word [playerY]
+        jmp update_snake_body
+    move_down:
+        inc word [playerY]
+        jmp update_snake_body
+    move_left:
+        dec word [playerX]
+        jmp update_snake_body
+    move_right:
+        inc word [playerX]
+
+    update_snake_body:
+        ;;each cell is taking the place of its previous cell
+        imul bx,[snakeLength],2
+        .update_loop:
+            mov ax,[SNAKEXARRAY-2+bx]
+            mov word [SNAKEXARRAY+bx],ax
+            mov ax,[SNAKEYARRAY-2+bx]
+            mov word [SNAKEYARRAY+bx],ax
+
+            dec bx
+            dec bx
+        jnz .update_loop 
+    
+    ;;Update head in the snake array
+    mov ax,[playerX]
+    mov word [SNAKEXARRAY],ax
+    mov ax,[playerY]
+    mov word [SNAKEYARRAY],ax
+
+
+    get_player_input:
+    mov bl,[direction]
+    
+    mov ah,1
+
+
     delay_loop:
         mov bx,[TIMER]
         inc bx
         inc bx
+
         .delay:
             cmp [TIMER],bx
             jl .delay
