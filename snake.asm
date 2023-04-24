af2cb3e8dc523954b92644d1f669ca1244883154
Aryan Arora <aryanarora.w1@gmail.com>
2023-04-24 10:06:31 +0530

    added snake head & delay

--- snake.asm
+++ snake.asm
@@ -10,7 +10,7 @@ SCREENH = 25
 WINCOND = 10
 BGCOLOR = 03020h
 APPLECOLOR = 4020h
-SNAKECOLOR = 2020h
+SNAKECOLOR = 7020h
 TIMER = 046Ch ;timer ticks handled in bios data area at this location in ram
 SNAKEXARRAY = 1000h
 SNAKEYARRAY = 2000h
@@ -42,9 +42,39 @@ setup_game:
 game_loop:
     mov ax,BGCOLOR
     xor di,di
-    mov cx,SCREENW*SCREENW
+    mov cx,SCREENW*SCREENH
     rep stosw ;(mov es:di(destination index),ax and increase di) cx times
 
+    ;;Draw snake
+    xor bx,bx
+    mov cx,[snakeLength]
+    mov ax,SNAKECOLOR
+    .snake_loop:
+        imul di,[SNAKEYARRAY+bx],SCREENW*2
+        imul dx,[SNAKEXARRAY+bx],2
+        add di,dx
+        stosw
+        inc bx
+        inc bx
+    loop .snake_loop
+
+    ;;Draw apple
+    imul di,[appleY],SCREENW*2
+    imul dx,[appleX],2
+    add di,dx
+    mov ax, APPLECOLOR
+    stosw
+
+    delay_loop:
+        mov bx,[TIMER]
+        inc bx
+        inc bx
+        .delay:
+            cmp [TIMER],bx
+            jl .delay
+
+
+jmp game_loop
 
 ;; BOOTSECTOR PADDING
 times 510 - ($-$$) db 0 ; fill in zeros 510-(current bytes-top byte) times
